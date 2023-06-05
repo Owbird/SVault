943a0d82950607c2692014f63a7e3d61902f1583
owbird <owbirdphorccuo@gmail.com>
2023-06-05 00:18:32 +0000

    Chore: Update engine builds

--- build_engines.sh
+++ build_engines.sh
@@ -3,3 +3,5 @@ INSTALLER="/env/bin/pyinstaller"
 
 ${CRPTIFY_DIR}${INSTALLER} --name=ee1 --onefile ${CRPTIFY_DIR}/encrypt/encryptor.py
 ${CRPTIFY_DIR}${INSTALLER} --name=ee2 --onefile ${CRPTIFY_DIR}/decrypt/decryptor.py
+
+mv dist/* build/bin/
