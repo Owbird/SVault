0281553e9530cca226017811d6a432a245c6c044
owbird <owbirdphorccuo@gmail.com>
2023-06-05 00:16:53 +0000

    Feat: Engine build files added

---
+++ build_engines.sh
@@ -0,0 +1,5 @@
+CRPTIFY_DIR="lib/cryptify"
+INSTALLER="/env/bin/pyinstaller"
+
+${CRPTIFY_DIR}${INSTALLER} --name=ee1 --onefile ${CRPTIFY_DIR}/encrypt/encryptor.py
+${CRPTIFY_DIR}${INSTALLER} --name=ee2 --onefile ${CRPTIFY_DIR}/decrypt/decryptor.py
